43b5aa45c0c029df0c5c48bee0ffe14ba9d63205
riad <riad@gmail.com>
2022-04-24 00:06:08 +0000

    fixing Serialize cylclic datastructure

--- app/screens/ViewPostScreen.js
+++ app/screens/ViewPostScreen.js
@@ -1,26 +1,7 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React, { useEffect ,useState} from 'react'
 import useGetPosts from '../hooks/useGetPosts'
-const postd={
-  "id": "60d21bd367d0d8992e610e25",
-  "image": "https://img.dummyapi.io/photo-1556877986-d40833ec88b4.jpg",
-  "likes": 6,
-  "link": "https://www.behance.net/claudia_udrea",
-  "tags": [
-      "dog",
-      "canine",
-      "animal"
-  ],
-  "text": "Being a proud owner of a frenchie... That means lovely walks and true bonding.. I just love to capture the beauty of nature and my perfect \"batdog\" <3  shallow focus photo of short-coated black dog",
-  "publishDate": "2020-05-19T02:16:25.689Z",
-  "owner": {
-      "id": "60d0fe4f5311236168a109d4",
-      "title": "mr",
-      "firstName": "Valentin",
-      "lastName": "Ortega",
-      "picture": "https://randomuser.me/api/portraits/med/men/3.jpg"
-  }
-}
+
 const ViewPostScreen = ({ route,navigation }) => {
   const {post_id:id}=route.params
   console.log(id)
@@ -34,7 +15,6 @@ const ViewPostScreen = ({ route,navigation }) => {
     setIsLoading(false)
      setPost(fetchedPosts)
      
-     //return ()=>setPost(null)
 },[])
 
 
